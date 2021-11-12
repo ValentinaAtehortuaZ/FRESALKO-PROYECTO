d86e17ea6e4194c97bff8e65e4a7ceb1ce10449a
ValentinaAtehortuaZ <valentinaatehortuaz@gmail.com>
2021-11-11 20:26:23 -0500

    CC

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email= $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
+$message .= "Su apellido es: " . $apellido. " \r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'fresalko@gmail.com';
+$asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:Index.html");?>
